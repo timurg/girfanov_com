68226eef96ab0e7884142ba3cdd22828a88470db
Timur Girfanov <girfanov@outlook.com>
2022-04-26 10:07:49 +0500

    initial commit

---
+++ bot/settings.php
@@ -0,0 +1,3 @@
+<?php
+define('C_REST_WEB_HOOK_URL','https://office.elearningrb.ru/rest/397/pygs1w72hcnikg9m/');//url on creat Webhook
+define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
