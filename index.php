28ea087e84e49bff60424f2ed212aefcfcca3847
Timur Girfanov <girfanov@outlook.com>
2022-04-27 08:41:25 +0500

    Added font

--- index.php
+++ index.php
@@ -2,19 +2,14 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta name="title" content="Pixel Animation HTML5/Javascript" />
-    <style type="text/css">
-        #myCanvas { width: 100%; margin-bottom: -7px; }
-        body { background-color: #000; }
-    </style>
-
-    <title>Pixel Animation HTML5/Javascript</title>
+    <title>Well come</title>
 </head>
 <body>
 <div id="container">
-    <canvas id="myCanvas" width="1280" height="720" >
+    <canvas id="mainCanvas" width="1280" height="720" >
     </canvas>
 </div>
-<script src="/js/pixelanimation_js/pixelanimation.js"></script>
+<script src="js/two.js"></script>
+<script src="js/scene.js"></script>
 </body>
 </html>
